58c23f3b131f02afb1e2dcd2fa2c88060745db5e
Marcos Taron <marcostaron@MacBook-Air-de-Marcos.local>
2020-09-06 23:52:34 -0300

    add config

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const TOKEN_API = 'BQCY7WypuFnZFIF4Z8FdFjpb1E6gI3JRILMozrsYJVzdSQUHLvz3_3HNelejhnmh-emLhZRD20-ZNz4-RXH5UhsF_lNRBItlrNtSzhvKN0CF-UhcliT_ifHQntZZEqTjVGTRgOrq31anzCuuSORPpZuS_9hXyjLO7qNMtauEmzZfKFa2VA3T4twtvSGRttQgmdfqwq7KjWK4Tye1aUX_ccbTfBNgcqGJZ3hQNF83lRCX201-qHLOd8tQOK4gGo07C3QGwbJ7hig6TA';
+
+export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
