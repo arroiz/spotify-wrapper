40ad4381965aa320a725e7ad599c04ab4e1b3004
Marcos Taron <marcostaron@MacBook-Air-de-Marcos.local>
2020-09-07 00:00:47 -0300

    add resources index

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+} from './search';
+
+import {
+  getAlbum,
+  getAlbumTracks,
+  getAlbums,
+} from './album';
+
+export default {
+  getAlbum,
+  getAlbumTracks,
+  getAlbums,
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+};
